9a7db77c232da7154568f9a70b192fd43cbae4a7
robberbadeia <robber.badeia@gmail.com>
2024-12-17 04:45:30 -0800

    User model implementation

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -27,3 +27,41 @@ export interface UserProps {
     bs: "harness real-time e-markets";
   };
 }
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+export interface UserData {
+  id: 1;
+  name: "Leanne Graham";
+  username: "Bret";
+  email: "Sincere@april.biz";
+  address: {
+    street: "Kulas Light";
+    suite: "Apt. 556";
+    city: "Gwenborough";
+    zipcode: "92998-3874";
+    geo: {
+      lat: "-37.3159";
+      lng: "81.1496";
+    };
+  };
+  phone: "1-770-736-8031 x56442";
+  website: "hildegard.org";
+  company: {
+    name: "Romaguera-Crona";
+    catchPhrase: "Multi-layered client-server neural-net";
+    bs: "harness real-time e-markets";
+  };
+}
+
+export interface UserModalProps {
+  onClose: () => void;
+  onSubmit: (user: UserData) => void;
+}
